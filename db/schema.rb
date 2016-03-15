41678552fefe02788fc585243f0396de392d105b
Jose Fernando <jfgonzal@eagle.fgcu.edu>
2016-03-15 03:26:00 -0700

    first raisl commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222031135) do
+ActiveRecord::Schema.define(version: 20160309032830) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",      limit: 50, null: false
-    t.string   "password",   limit: 50, null: false
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.string   "password"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
